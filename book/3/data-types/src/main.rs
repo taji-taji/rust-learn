24cf80df633eaef3bbb3ff57fbffe22c7a4e210a
taji-taji <y.tajika529@gmail.com>
2018-09-06 21:24:01 +0900

    Floating Point Types

--- book/3/data-types/src/main.rs
+++ book/3/data-types/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    let guess: u32 = "42".parse().expect("Not a number!");
+    // Integer Types
+    let _guess: u32 = "42".parse().expect("Not a number!");
+
+    // Floating-Point Types
+    let _x = 2.0; // default f64
+
+    let _y: f32 = 3.0;
+
+    // Numeric operation
+    let sum = 5 + 10;
+    println!("sum: {}", sum);
+
+    let difference = 95.5 - 4.3;
+    println!("difference: {}", difference);
+
+    let product = 4 * 30;
+    println!("product: {}", product);
+
+    let quotient = 56.7 / 32.2;
+    println!("quotient: {}", quotient);
+
+    let remainder = 43 % 5;
+    println!("remainder: {}", remainder);
 }
