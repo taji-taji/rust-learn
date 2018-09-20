7c3b305c9f4b74264d1f04e4736eaac4f68d46e6
taji-taji <y.tajika529@gmail.com>
2018-09-20 19:19:47 +0900

    Chapter4: Ways Variables and Data Interact: Clone

--- book/4/what-is-ownership/src/main.rs
+++ book/4/what-is-ownership/src/main.rs
@@ -22,4 +22,11 @@ fn main() {
         // println!("{}, world!", s1);
     }
 
+    {
+        let s1 = String::from("hello");
+        let s2 = s1.clone();
+
+        println!("s1 = {}, s2 = {}", s1, s2);
+    }
+
 }
